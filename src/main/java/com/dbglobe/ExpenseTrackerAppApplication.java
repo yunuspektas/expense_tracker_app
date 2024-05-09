56a69e258a7ec64cd87153b9ae9745b24fd16835
yunus pektas <yunuspektas@protonmail.com>
2024-05-09 16:16:45 +0300

    expense_tracker_app

---
+++ src/main/java/com/dbglobe/ExpenseTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.dbglobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerAppApplication.class, args);
+	}
+
+}
